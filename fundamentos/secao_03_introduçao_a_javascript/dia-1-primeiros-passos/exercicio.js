bf64024769d9b7f0e44f1f1d23a065b87dae912f
Mateus Oliveira <MATEUS17333@GMAIL.COM>
2023-01-25 18:31:02 -0300

    primeiro exercicio dia 1

---
+++ fundamentos/secao_03_introduçao_a_javascript/dia-1-primeiros-passos/exercicio.js
@@ -0,0 +1,12 @@
+let num01 = 12;
+let num02 = 6;
+soma = num01 + num02;
+  console.log(soma);
+subtracao = num01 - num02;
+  console.log(subtracao)
+multiplicacao = num01 * num02;
+  console.log(multiplicacao)
+divisao = num01 / num02;
+   console.log(divisao) 
+modulo = num01 % num02;
+console.log(modulo)    
