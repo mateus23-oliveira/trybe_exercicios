b09930de8a6f9a248899c0768e8ff962d1109661
Mateus Oliveira <MATEUS17333@GMAIL.COM>
2023-01-25 22:35:03 -0300

    criando exercicios 4

--- fundamentos/secao_03_introduçao_a_javascript/dia-1-primeiros-passos/exercicio.js
+++ fundamentos/secao_03_introduçao_a_javascript/dia-1-primeiros-passos/exercicio.js
@@ -30,9 +30,63 @@ if(num01 > num02 && num01 > num03) {
  
 let comparacao = 0;
   if(comparacao > 0) {
-       console.log('positivo')
+       console.log('positivo');
 }  else if( comparacao < 0) {
-    console.log('negativo')
+    console.log('negativo');
 } else {
-    console.log('zero')
+    console.log('zero');
 }
+
+let anguloA = 90;
+let anguloB = 60;
+let anguloC = 30;
+
+if(anguloA + anguloB + anguloC == 180) {
+   console.log(true);
+} else {
+    console.log(false)
+} 
+const chessPiece = 'bispo';
+
+switch (chessPiece) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+};
+
+ let porcentagem = 63;  
+if(porcentagem >= 90){
+    console.log('A')
+} else if(porcentagem >= 80) {
+    console.log('B')
+} else if(porcentagem >= 70) {
+    console.log('C')
+} else if(porcentagem >= 60) {
+    console.log('D')
+} else if(porcentagem >= 50) {
+    console.log('E')
+} else if(porcentagem < 50) {
+    console.log('F')
+} else if(porcentagem > 100 || porcentagem < 0) {
+    console.log('erro nota imvalida')
+}
+  
+
