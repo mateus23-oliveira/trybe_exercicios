13f51a0952f7b45b26a200addcf1678b7afab336
Mateus Oliveira <MATEUS17333@GMAIL.COM>
2023-01-26 21:29:42 -0300

    criando repeticao

--- fundamentos/secao_03_introduçao_a_javascript/dia-2-arays-e-loop-for/exercicio.js
+++ fundamentos/secao_03_introduçao_a_javascript/dia-2-arays-e-loop-for/exercicio.js
@@ -1,4 +1,24 @@
-let intervalo = 0; 
-for (i = 0; i <= 150; i++)
-intervalo += intervalo[i] % 3 == 0;
-console.log(intervalo);
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(index = 0; index < numbers.length; index += 1) {
+   console.log(numbers[index])
+}
+ soma = 0;
+for(index = 0; index < numbers.length; index += 1) {
+   soma += numbers[index];
+}
+    console.log(soma)
+ 
+let mediaAritmetica = 0;
+ for(index = 0; index < numbers.length; index += 1) {
+   mediaAritmetica += numbers[index];
+ }
+   mediaAritmetica = mediaAritmetica / numbers.length; 
+   console.log(mediaAritmetica)
+   if(mediaAritmetica > 20) {
+    console.log('valor maior que 20.')
+   } else {
+    console.log('valor menor ou igual a 20')
+   }
+ 
+   
